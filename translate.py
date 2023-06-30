3119ae61add0edfd0c7927dc71f6ac8334281ca4
ny5us <133588054+ny5us@users.noreply.github.com>
2023-06-30 10:09:54 -0700

    Update translate.py
    Fully functioning translations from English to Spanish

--- translate.py
+++ translate.py
@@ -1,9 +1,21 @@
 import tkinter as tk
 from tkinter.ttk import Notebook
+import requests
 
 class Translate (tk.Tk):
-    def translate():
-        print("Translate")
+
+    def translate(self, target_language = "es", text = None):
+        if not text:
+            text = self.english_entry.get(1.0, tk.END)
+        url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}".format("en", target_language, text)
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            print(response.json())
+            self.spanish_translation.set(response.json()[0][0][0])
+        except Exception as e:
+            print(e)
         
     def __init__(self):
 
@@ -23,6 +35,15 @@ class Translate (tk.Tk):
         self.translate_button.pack(side= tk.BOTTOM, fill = tk.X)
 
         self.notebook.add(english_tab, text = "English")
+
+        spanish_tab = tk.Frame(self.notebook)
+        self.notebook.add(spanish_tab, text = "Spanish")
+        
+        self.spanish_translation = tk.StringVar(spanish_tab)
+        self.spanish_translation.set("No Translation")
+
+        self.spanish_label = tk.Label(spanish_tab, textvar = self.spanish_translation)
+        self.spanish_label.pack(side = tk.TOP, fill = tk.BOTH, expand = 1)
 
 if __name__ == "__main__":
     translate = Translate()
