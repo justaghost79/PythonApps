befbd7298d36177345561d1a56eeb2d5368681ba
ny5us <133588054+ny5us@users.noreply.github.com>
2023-07-02 08:08:09 -0700

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -13,11 +13,23 @@ menu = Menu(root)
 file = Menu(menu, tearoff = 0)
 
 def Open():
-    print("Hello")
+    root.filename = filedialog.askopenfilename(
+        initialdir="/",
+        title = "Select File",
+        filetypes = (("jpeg files","*.jpg"),("all files","*.*"))
+    )
+    file = open(root.filename)
+    text.insert("end", file.read())
+
 def Save():
-    print("Save")
+    pass
 def SaveAs():
-    print("Save As")
+    root.filename = filedialog.asksaveasfile(mode="w", defaultextension = ".txt")
+    if root.filename is None:
+        return
+    file_tosave = str(text.get(1.0, END))
+    root.filename.write(file_to_save)
+    root.filename.close()
 def Exit():
     print("Exit")
 file.add_command(label="Open", command=open)
