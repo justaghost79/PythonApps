9c86ec14c0f16344d781e4a244d201e846a8ba68
ny5us <133588054+ny5us@users.noreply.github.com>
2023-07-03 14:46:23 -0700

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
 from tkinter.scrolledtext import ScrolledText
 from tkinter import *
+
+
 
 root = tk.Tk()
 
@@ -16,10 +19,10 @@ def Open():
     root.filename = filedialog.askopenfilename(
         initialdir="/",
         title = "Select File",
-        filetypes = (("jpeg files","*.jpg"),("all files","*.*"))
+        filetypes = (("jpeg files","*.jpg"), ("all files","*.*"))
     )
     file = open(root.filename)
-    text.insert("end", file.read())
+    Text.insert("end", file.read())
 
 def Save():
     pass
@@ -27,11 +30,17 @@ def SaveAs():
     root.filename = filedialog.asksaveasfile(mode="w", defaultextension = ".txt")
     if root.filename is None:
         return
-    file_tosave = str(text.get(1.0, END))
+    file_to_save = str(Text.get(1.0, END))
     root.filename.write(file_to_save)
     root.filename.close()
 def Exit():
-    print("Exit")
+    message = message.box.askquestion("Notepad", "Do you want to save?" )
+    if message == "yes":
+        SaveAs()
+    else:
+        root.destroy()
+
+
 file.add_command(label="Open", command=open)
 file.add_command(label="Save", command=Save)
 file.add_command(label="Save As", command=SaveAs)
