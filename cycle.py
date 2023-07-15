b2358726ec28a63b40a9def99a1198e4ff2eb853
ny5us <133588054+ny5us@users.noreply.github.com>
2023-07-15 15:12:45 -0700

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -16,4 +16,6 @@ class Node:
     head.next = Node(2)
     head.next.next= Node(3)
     head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = head.next.next.next.next
     has_cycle(head)
