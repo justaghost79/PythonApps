deb91d18f6f6e7c330bc79db98828fdaaebce222
ny5us <133588054+ny5us@users.noreply.github.com>
2023-07-19 17:29:32 -0700

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1>Welcome to the jungle, baby!</h1><p>Under Construction</p>"
+    return "<h1>Welcome to the jungle, baby!</h1><p>Blogsite Under Construction</p>"
 @app.route('/aboutMe')
 def aboutMe():
     return "About me!"
